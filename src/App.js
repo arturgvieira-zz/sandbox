4c5d24c5446820ada859f8c28ed5b10e3c2c6153
arturgvieira <vieira.artur.g@gmail.com>
2018-12-01 02:04:06 -0800

    Add basic structure for Main Component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 // Apollo Client Configuration
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
-import Main from './main';
+import Main from './feature';
 
 const client = new ApolloClient({
     uri: 'http://localhost:8080/graphql'
